5ccc2f94b6290831fac8a2b6287cf215d114e608
sanwarhossain5 <sonymix5000@gmail.com>
2022-11-13 00:10:03 +0600

    data loaded

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ap9xvzb.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
@@ -39,7 +39,24 @@ async function run(){
          
          res.send(service)
         })
+
+app.post('/service' , async(req,res)=>{
+  const service = req.body;
+  const result = await serviceCollection.insertOne(service)
+  res.send(result)
+})
+
+
+
+
+
  }
+
+ 
+
+
+
+
  finally{
 
  }
