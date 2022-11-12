8cb6e122e2363d3441bbf96931b5d80822b7655c
sanwarhossain5 <sonymix5000@gmail.com>
2022-11-12 16:04:51 +0600

    data added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 require('dotenv').config()
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
@@ -15,17 +16,45 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ap9xvzb.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+ try{
+        const serviceCollection = client.db('doctorHome').collection('services')
+        app.get('/services', async(req, res) =>{
+             const query = {}
+             const cursor = serviceCollection.find(query).limit(3)
+             const services = await cursor.toArray()
+             res.send(services)
+        })
+        app.get('/service', async(req, res) =>{
+          const query = {}
+          const cursor = serviceCollection.find(query)
+          const services = await cursor.toArray()
+          res.send(services)
+        })
+        app.get('/service/:id' , async (req,res)=>{
+          const id = req.params.id;
+         const query = {_id: ObjectId(id)};
+         const service = await serviceCollection.findOne(query);
+         
+         res.send(service)
+        })
+ }
+ finally{
+
+ }
+ 
+
+}
+run().catch(err => console.error(err))
 
 
-app.get('/' , (req,res) =>{
-  res.send('doctor is coming')
+
+
+app.get('/' , (req,res)=>{
+  res.send('doctor home is running')
 })
 
-app.listen(port , () =>{
-  console.log(`doctor home server is running ${port}`);
+app.listen(port, () =>{
+  console.log(`doctor home server running on ${port}`);
 })
